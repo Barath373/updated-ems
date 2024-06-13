bafcfd45d3891a1ac43fe0e970f1a43e9752b9d6
Barath373 <barath.erulappan@cogniwide.com>
2024-06-13 09:40:21 +0530

    Delete ems/Backend/Backend/EventManagement directory

--- ems/Backend/Backend/EventManagement/src/main/java/com/example/EventManagement/Repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.EventManagement.Repository;
-import com.example.EventManagement.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, String> {
-    User findByName(String name);
-}
